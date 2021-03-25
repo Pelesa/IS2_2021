97c566285a75cfdfd02e579ced611b433126c308
Pepin <pelesarri@gmail.com>
2021-03-25 11:40:38 +0100

    Maven v0.02

--- practica3/src/main/java/aplicacion/Alarma.java
+++ practica3/src/main/java/aplicacion/Alarma.java
@@ -1,5 +1,12 @@
 package aplicacion;
 
-public class Alarma {
+import java.util.Date;
 
+public class Alarma {
+	String id;
+	Date hora;
+	public Alarma(String id, Date hora) {
+		this.id=id;
+		this.hora=hora;
+	}
 }
