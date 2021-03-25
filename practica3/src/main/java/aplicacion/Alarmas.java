7b324b9a8c9bce3a8a27329443246d2e46ca316e
Pepin <pelesarri@gmail.com>
2021-03-25 11:55:56 +0100

    Maven v0.05

--- practica3/src/main/java/aplicacion/Alarmas.java
+++ practica3/src/main/java/aplicacion/Alarmas.java
@@ -2,4 +2,47 @@ package aplicacion;
 
 public class Alarmas {
 	
+	Alarma alarmas[];
+	
+	
+	void anhadeAlarma(Alarma a) {
+		
+	}
+	public boolean eliminaAlarma() {
+		return false;
+	}
+	public Alarma alarmaMasProxima() {
+		return null;
+	}
+	void activaAlarma(Alarma a) {
+		
+	}
+	public Alarma[] alarmasActivadas(){
+		return null;
+	}
+	public Alarma[] alarmasDesactivadas(){
+		return null;
+	}
+	void activarMelodia() {
+		
+	}
+	void desactivarMelodia() {
+		
+	}
+	void NuevaAlarma() {
+		
+	}
+	void Apagar() {
+		
+	}
+	void AlarmaOff() {
+		
+	}
+	void AlarmaOn() {
+		
+	}
+	void BorraAlarma() {
+		
+	}
+	
 }
