ed2a915e0b2cf9cd4a48864ed5e3794bad7ec5d9
Pelesa <53566770+Pelesa@users.noreply.github.com>
2021-04-30 00:09:48 +0200

    CalculaPrecio

--- practica4/src/main/java/es/unican/is2/seguros/model/PruebaEnum.java
+++ practica4/src/main/java/es/unican/is2/seguros/model/PruebaEnum.java
@@ -1,17 +1,36 @@
 package es.unican.is2.seguros.model;
 
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeFormatter;
+
 public class PruebaEnum {
 	 public static void main (String[ ] Args) {
 
-		 
+		 	LocalDate fechaUltimoSiniestro=LocalDate.parse("11/04/2020", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
 	       Cobertura cobertura1 = Cobertura.valueOf("TERCEROS");
 
 	       System.out.println ("La Cobertura elegida por el usuario es " + cobertura1.toString() + "\n " +
-
 			cobertura1.getCobertura() + " y con un precio base de " + cobertura1.getCosteBase() + " euros");
-
-	 
+	       
+	       
+	       System.out.println(fechaUltimoSiniestro);
+	       System.out.println(LocalDate.now());
+	       
+			double incSiniestros=0;
+			if(fechaUltimoSiniestro!=null) {
+				Long since = ChronoUnit.DAYS.between(fechaUltimoSiniestro,LocalDate.now());
+				double anhos=since/365;
+				if(anhos<=1) {
+					incSiniestros=200;
+				}else {
+					if(1<anhos&&anhos<=3) {
+						incSiniestros=50;
+					}
+				}
+			}
 
 	 }      
 
--- practica4/src/main/java/es/unican/is2/seguros/model/Seguro.java
+++ practica4/src/main/java/es/unican/is2/seguros/model/Seguro.java
@@ -3,6 +3,7 @@ package es.unican.is2.seguros.model;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Seguro {
 	private LocalDate fechaUltimoSiniestro;
@@ -23,16 +24,21 @@ public class Seguro {
 	public void setPrecio() {
 		double base=cobertura.getCosteBase();
 		double incPotencia;
-		if(90<=potenciaCV&&potenciaCV<=110) {
-			incPotencia=base*0.05;
+		if(potenciaCV<90) {
+			incPotencia=0;
 		}else {
-			incPotencia=base*0.2;
+			if(90<=potenciaCV&&potenciaCV<=110) {
+				incPotencia=base*0.05;
+			}else {
+				incPotencia=base*0.2;
+			}
 		}
+
 		
 		double incSiniestros=0;
 		if(fechaUltimoSiniestro!=null) {
-			Duration since= Duration.between(fechaUltimoSiniestro,LocalDate.now());
-			double anhos=since.toDays()/365;
+			Long since = ChronoUnit.DAYS.between(fechaUltimoSiniestro,LocalDate.now());
+			double anhos=since/365;
 			if(anhos<=1) {
 				incSiniestros=200;
 			}else {
